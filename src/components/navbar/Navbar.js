5ccdb1e35fe3d2b04a3da65d72b563c9181a3d15
tahir <tahiraydin06@gmail.com>
2021-12-22 13:22:49 +0300

    finish recipe app

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React,{useState} from 'react';
+import {Hamburger, Logo, Menu, MenuLink, Nav} from "./NavbarStyle";
+
+
+const Navbar = () => {
+    const[isOpen,setIsopen]=useState(false)
+    return (
+        <div>
+            <Nav>
+                <Logo to="/">
+
+                    <i>{"Peerlog"}</i><span>recipe</span>
+
+                </Logo>
+
+                <Hamburger onClick={() => setIsopen(!isOpen)}>
+                    <span />
+                    <span />
+                    <span />
+                </Hamburger>
+
+                <Menu isOpen={isOpen}>
+                    <MenuLink to="/about">About</MenuLink>
+                    <MenuLink
+                      to={{ pathname: "https://github.com/tahiraydin" }}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Github
+                    </MenuLink>
+                    <MenuLink to="/login">Logout</MenuLink>
+                </Menu>
+
+            </Nav>
+        </div>
+    )
+}
+
+export default Navbar
